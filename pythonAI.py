e142748929f04cd9b2585c5d606baaed1fd0b1c9
Kelly Click <kclick91@gmail.com>
2023-09-20 17:14:41 -0400

    Corrections and more training data for MLB Box scores

--- pythonAI.py
+++ pythonAI.py
@@ -40,7 +40,9 @@ output_size = int(input("Enter the output size."))#Example is 1 so enter 1 for t
 
 # Games of 9/9/2023
 # Brewers v Yankees, Mets v Twins, Diamondbacks vs Cubs, Royals vs Blue Jays, Dodgers vs Nationals, White Sox vs Tiger, Cardinals vs Reds, Athletics vs Rangers, Padres vs Astros
-# Pirates v Braves, Rockies vs Giants, Guardians vs Angels 
+# Pirates v Braves, Rockies vs Giants, Guardians vs Angels
+# Games of 9/19/2023
+# Angels vs Rays, Twins vs Reds, Mets vs Marlins, White Sox vs Nationals
 # Score and inning without consideration of anything about the specific teams
 EARLIER_GAMES = np.array([[0,0,1], [0,0,2], [0,0,3], [2,2,4],[2,2,5],[2,2,6], [2,2,7], [5,2,8], [9,2,9], [2,0,1], [2,2,2], [2,3,3], [2,3,4], [2,3,5], [2,3,6], 
 [2,7,7], [4,8,8], [4,8,9], [0,0,1], [0,0,2], [0,1,3], [0,1,4],[1,1,5],[1,1,6],[1,1,7], [1,1,8], [1,1,9], [2,1,10], [0,0,1], [0,0,2], [0,0,3], [0,1,4], [1,3,5], 
@@ -48,10 +50,12 @@ EARLIER_GAMES = np.array([[0,0,1], [0,0,2], [0,0,3], [2,2,4],[2,2,5],[2,2,6], [2
 [0,1,4], [0,1,5], [0,2,6], [0,3,7], [1,3,8], [1,3,9], [1,0,1], [1,3,2], [3,3,3], [4,3,4], [4,3,5], [4,3,6], [4,3,7], [4,3,8], [4,3,9], [0,0,1], [0,0,2],[0,0,3],
 [0,0,4],[0,0,5],[0,2,6],[2,3,7],[2,3,8],[2,3,9],[0,0,1],[0,0,2],[0,1,3],[4,2,4],[4,7,5],[5,7,6],[5,7,7],[5,7,8],[5,7,9],[0,0,1], [0,0,2],[3,1,3],[3,3,4],
 [6,3,5],[7,3,6],[7,4,7],[7,4,8],[8,4,9],[0,2,1],[0,2,2],[0,2,3],[0,6,4],[0,6,5],[0,9,6],[1,9,7],[1,9,8],[1,9,9],[1,2,1],[1,2,2],[2,3,3],[2,4,4],[2,4,5],[2,4,6],
-[2,4,7],[2,6,8],[2,6,9]])
+[2,4,7],[2,6,8],[2,6,9],[0,2,1],[0,2,2],[0,2,3],[1,2,4],[1,2,5],[1,2,6],[1,2,7],[2,6,8],[2,6,9],[0,0,1],[1,0,2],[1,0,3],[2,0,4],[2,0,5],[4,0,6],[6,0,7],[7,0,8],[7,0,9],
+[0,0,1],[0,0,2],[1,1,3],[1,1,4],[1,2,5],[1,3,6],[1,3,7],[1,3,8],[3,4,9],[0,0,1],[0,0,2],[0,0,3],[1,0,4],[1,0,5],[1,1,6],[2,4,7],[2,4,8],[3,4,9]])
 
 EARLIER_GAMES_RESULTS = np.array([[1],[1],[1],[1],[1],[1],[1],[1],[1],[0],[0],[0],[0],[0],[0],[0],[0],[0],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],
  [0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[1],[1],[1],[1],[1],[1],[1],[1],[1],
+ [0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[1],[1],[1],[1],[1],[1],[1],[1],[1],[0],[0],[0],[0],[0],[0],[0],[0],[0],
  [0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[1],[1],[1],[1],[1],[1],[1],[1],[1],[0],[0],[0],[0],[0],[0],[0],[0],[0],
  [0],[0],[0],[0],[0],[0],[0],[0],[0]])
 
@@ -96,27 +100,27 @@ epochs = 10000
 # print("Predicted Output:")
 # print(predicted_output)
 
-
 learning_rate = float(input("Enter the learning rate."))
 epochs = int(input("Enter the number of epochs."))
 
+np.random.seed(42)  # For reproducibility
+weights_input_hidden1 = np.random.uniform(size=(input_size, hidden1_size))
+bias_hidden1 = np.zeros((1, hidden1_size))
+weights_hidden1_hidden2 = np.random.uniform(size=(hidden1_size, hidden2_size))
+bias_hidden2 = np.zeros((1, hidden2_size))
+weights_hidden2_output = np.random.uniform(size=(hidden2_size, output_size))
+bias_output = np.zeros((1, output_size))
+print("Weights: ", weights_input_hidden1)
 def train(EARLY, EARLYRESULTS, LATER):
-    np.random.seed(42)  # For reproducibility
-    weights_input_hidden1 = np.random.uniform(size=(input_size, hidden1_size))
-    bias_hidden1 = np.zeros((1, hidden1_size))
-    weights_hidden1_hidden2 = np.random.uniform(size=(hidden1_size, hidden2_size))
-    bias_hidden2 = np.zeros((1, hidden2_size))
-    weights_hidden2_output = np.random.uniform(size=(hidden2_size, output_size))
-    bias_output = np.zeros((1, output_size))
+    
     for epoch in range(epochs):
         # Forward propagation
-        hidden1_layer_input = np.dot(EARLY, weights_input_hidden1) + bias_hidden1
+        hidden1_layer_input = np.dot(EARLY, globals()["weights_input_hidden1"]) + globals()["bias_hidden1"]
         hidden1_layer_output = sigmoid(hidden1_layer_input)
+        hidden2_layer_input = np.dot(hidden1_layer_output, globals()["weights_hidden1_hidden2"]) + globals()["bias_hidden2"]
 
-        hidden2_layer_input = np.dot(hidden1_layer_output, weights_hidden1_hidden2) + bias_hidden2
         hidden2_layer_output = sigmoid(hidden2_layer_input)
-
-        output_layer_input = np.dot(hidden2_layer_output, weights_hidden2_output) + bias_output
+        output_layer_input = np.dot(hidden2_layer_output, globals()["weights_hidden2_output"]) + globals()["bias_output"]
         output_layer_output = sigmoid(output_layer_input)
 
         # Calculate the loss
@@ -124,23 +128,23 @@ def train(EARLY, EARLYRESULTS, LATER):
 
         # Backpropagation
         d_output = loss * sigmoid_derivative(output_layer_output)
-        error_hidden2_layer = d_output.dot(weights_hidden2_output.T)
+        error_hidden2_layer = d_output.dot(globals()["weights_hidden2_output"].T)
         d_hidden2_layer = error_hidden2_layer * sigmoid_derivative(hidden2_layer_output)
-        error_hidden1_layer = d_hidden2_layer.dot(weights_hidden1_hidden2.T)
+        error_hidden1_layer = d_hidden2_layer.dot(globals()["weights_hidden1_hidden2"].T)
         d_hidden1_layer = error_hidden1_layer * sigmoid_derivative(hidden1_layer_output)
 
         # Update weights and biases
-        weights_hidden2_output += hidden2_layer_output.T.dot(d_output) * learning_rate
-        bias_output += np.sum(d_output, axis=0, keepdims=True) * learning_rate
-        weights_hidden1_hidden2 += hidden1_layer_output.T.dot(d_hidden2_layer) * learning_rate
-        bias_hidden2 += np.sum(d_hidden2_layer, axis=0, keepdims=True) * learning_rate
-        weights_input_hidden1 += EARLY.T.dot(d_hidden1_layer) * learning_rate
-        bias_hidden1 += np.sum(d_hidden1_layer, axis=0, keepdims=True) * learning_rate
+        globals()["weights_hidden2_output"] += hidden2_layer_output.T.dot(d_output) * learning_rate
+        globals()["bias_output"] += np.sum(d_output, axis=0, keepdims=True) * learning_rate
+        globals()["weights_hidden1_hidden2"] += hidden1_layer_output.T.dot(d_hidden2_layer) * learning_rate
+        globals()["bias_hidden2"] += np.sum(d_hidden2_layer, axis=0, keepdims=True) * learning_rate
+        globals()["weights_input_hidden1"] += EARLY.T.dot(d_hidden1_layer) * learning_rate
+        globals()["bias_hidden1"] += np.sum(d_hidden1_layer, axis=0, keepdims=True) * learning_rate
     
     # Evaluate the trained model
-    hidden1_layer = sigmoid(np.dot(LATER, weights_input_hidden1) + bias_hidden1)
-    hidden2_layer = sigmoid(np.dot(hidden1_layer, weights_hidden1_hidden2) + bias_hidden2)
-    predicted_output = sigmoid(np.dot(hidden2_layer, weights_hidden2_output) + bias_output)
+    hidden1_layer = sigmoid(np.dot(LATER, globals()["weights_input_hidden1"]) + globals()["bias_hidden1"])
+    hidden2_layer = sigmoid(np.dot(hidden1_layer, globals()["weights_hidden1_hidden2"]) + globals()["bias_hidden2"])
+    predicted_output = sigmoid(np.dot(hidden2_layer, globals()["weights_hidden2_output"]) + globals()["bias_output"])
     
     print("Predicted Output:")
     print(predicted_output)
@@ -158,7 +162,8 @@ def MLBBoxScore():
         hidden2_layerEntered = sigmoid(np.dot(hidden1_layerEntered, weights_hidden1_hidden2) + bias_hidden2)
         predicted_outputEntered = sigmoid(np.dot(hidden2_layerEntered, weights_hidden2_output) + bias_output)
         print(predicted_outputEntered)
-      
+        
+        
 train(EARLIER_GAMES, EARLIER_GAMES_RESULTS, TEST_GAMES_TWO)
 MLBBoxScore()
 
